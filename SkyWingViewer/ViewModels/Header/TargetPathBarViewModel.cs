8b8563cfb96a5af7951afabcd79041d3162dcb34
asamichi <asamichi.dev@gmail.com>
2026-07-26 11:26:27 +0900

    [Add] パンくずリストの実装

--- SkyWingViewer/ViewModels/Header/TargetPathBarViewModel.cs
+++ SkyWingViewer/ViewModels/Header/TargetPathBarViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.IdentityModel.Tokens;
 using SkyWingViewer.Models;
 using SkyWingViewer.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Text;
 using System.Windows;
@@ -16,6 +19,20 @@ public partial class TargetPathBarViewModel : ObservableObject
     [ObservableProperty]
     public string? targetPath;
 
+
+    public class Breadcrumb
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public Breadcrumb(string name, string path)
+        {
+            Name = name;
+            Path = path;
+        }
+    }
+    //パンくずリスト
+    public ObservableCollection<Breadcrumb> BreadcrumbList { get; } = new();
+
     public TargetPathBarViewModel(TargetNavigationService targetNavigationService)
     {
         _targetNavigationService = targetNavigationService;
@@ -23,13 +40,15 @@ public partial class TargetPathBarViewModel : ObservableObject
 
         //イベント登録
         _targetNavigationService.TargetPathChanged += OnTargetPathChanged;
+        CreateBreadcrumb(TargetPath);
 
     }
 
-    //こちらは _model.TargetPathChanged に登録されるほう
+    //こちらは ナビゲーションサービスの TargetPathChanged に登録されるほう
     private void OnTargetPathChanged()
     {
         TargetPath = _targetNavigationService.Path;
+        CreateBreadcrumb(TargetPath);
     }
 
     //こちらは This.TargetPath が変更されたときに発火
@@ -41,4 +60,51 @@ public partial class TargetPathBarViewModel : ObservableObject
         _targetNavigationService.SetPath(value);
 
     }
+
+
+    // ここからパンくずリスト実装
+
+
+    //リストのボタン押下時
+    [RelayCommand]
+    private void TargetPathChange(string path)
+    {
+        OnTargetPathChanged(path);
+    }
+
+    private void CreateBreadcrumb(string path)
+    {
+        BreadcrumbList.Clear();
+        if (string.IsNullOrEmpty(path))
+        {
+            return;
+        }
+
+        DirectoryInfo directoryInfo = new DirectoryInfo(path);
+        Stack<Breadcrumb> stack = new();
+        
+
+        DirectoryInfo? current = directoryInfo;
+
+        //一旦末尾から stack に登録していって、最後に表示用のコレクションに stack からポップしていく
+
+        //親が無くなるまで親に進む
+        while(current != null)
+        {
+            string name = current.Name;
+            name = name.TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+
+            stack.Push(new Breadcrumb(name, current.FullName));
+            //BreadcrumbList.Add(new Breadcrumb(name, current.FullName));
+            current = current.Parent;
+        }
+
+        while (stack.Count > 0)
+        {
+            BreadcrumbList.Add(stack.Pop());
+        }
+
+    }
+
+
 }
--- SkyWingViewer/Views/Header/TargetPathBarView.xaml.cs
+++ SkyWingViewer/Views/Header/TargetPathBarView.xaml.cs
@@ -22,5 +22,29 @@ namespace SkyWingViewer.Views
         {
             InitializeComponent();
         }
+
+        // 空きスペースが押されたら TextBox に切り替えてフォーカス
+        private void BreadcrumbContainer_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            BreadcrumbContainer.Visibility = Visibility.Collapsed;
+            TargetPathTextBox.Focus();
+            TargetPathTextBox.SelectAll();
+        }
+
+        // フォーカスが外れたらパンくず表示に戻す
+        private void TargetPathTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            BreadcrumbContainer.Visibility = Visibility.Visible;
+        }
+
+        private void BreadcrumbScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            // 中身の横幅が変わった（パスが更新された、またはウィンドウ幅が変わった）時
+            if (e.ExtentWidthChange != 0)
+            {
+                // 常に右端までスクロールさせる
+                BreadcrumbScrollViewer.ScrollToRightEnd();
+            }
+        }
     }
 }
